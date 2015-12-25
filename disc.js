86e222f680634cf101540afa9d1d5e7c45767b5e
cyc <342930982@qq.com>
2015-12-25 13:30:09 +0800

    change

--- disc.js
+++ disc.js
@@ -1,43 +1,74 @@
-function get(url, data, callback) {
-    if(typeof data === "function"){
-        callback = data;
+(function(window){
+
+
+
+    function get(url, data, callback) {
+        if(typeof data === "function"){
+            callback = data;
+        }
+
+        if(typeof data === "object"){
+            url = data ? url + '?' + urlCode(data) : url;
+        }
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url);
+        xhr.onload = function () {
+            callback(JSON.parse(xhr.responseText));
+        }
+        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhr.send();
     }
 
-    if(typeof data === "object"){
-        url = data ? url + '?' + urlCode(data) : url;
+    function post(url, data, callback) {
+        if(typeof data === "function"){
+            callback = data;
+            data = null;
+        }
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', url);
+        xhr.onload = function () {
+            callback(JSON.parse(xhr.responseText));
+        };
+        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhr.setRequestHeader('content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(urlCode(data));
+    }
+    function urlCode(data) {
+        var str = '';
+        if (!data) {
+            return null;
+        }
+        for (var key in data) {
+            str += key + '='+ encodeURIComponent(data[key])+ '&';
+        }
+
+        return str.substring(0, str.length-1);
     }
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url);
-    xhr.onload = function () {
-        callback(JSON.parse(xhr.responseText));
-    }
-    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    xhr.send();
-}
+    function ccajax(option){
+        var defaults = {
+            method: "post",
+            url: '/',
+            callback: function(){},
+            data: {}
+        }
 
-function post(url, data, callback) {
-    if(typeof data === "function"){
-        callback = data;
-        data = null;
+        for(var name in option){
+            defaults[name] = option[name]
+        }
+
+        var xhr = new XMLHttpRequest();
+        xhr.open(defaults.method, defaults.url);
+        xhr.onload = function () {
+            defaults.callback(JSON.parse(xhr.responseText));
+        };
+        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhr.setRequestHeader('content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(urlCode(defaults.data));
     }
-    var xhr = new XMLHttpRequest();
-    xhr.open('post', url);
-    xhr.onload = function () {
-        callback(JSON.parse(xhr.responseText));
-    };
-    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    xhr.setRequestHeader('content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(urlCode(data));
-}
-function urlCode(data) {
-    var str = '';
-    if (!data) {
-        return null;
-    }
-    for (var key in data) {
-        str += key + '='+ encodeURIComponent(data[key])+ '&';
-    }
-    
-    return str.substring(0, str.length-1);
-}
+
+    window.get = get
+    window.post = post
+    window.ccajax = ccajax
+})(window)
